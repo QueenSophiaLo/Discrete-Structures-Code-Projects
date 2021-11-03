75aa903da03a45fea0c21840c53dcafdff0ad05c
QueenSophiaLo <75154780+QueenSophiaLo@users.noreply.github.com>
2021-11-02 21:53:08 -0400

    Update Draft_1.cpp

--- Factorial Digits (FAC)/Draft_1.cpp
+++ Factorial Digits (FAC)/Draft_1.cpp
@@ -1,14 +1,13 @@
-Project 1: Factorial Digits [FAC]
-Textbook Section: 2.9 & 7.35
+// Sophia Godfrey
+// Project 2: Factorial Digits [FAC]
+// C++ code
 
-Directions: Given a factorial n!.  Find the sum of its digits, and the number of trailing zeros (ie: the number of zeros at the end of the factorial). 
-Input: user chooses integer n
-, where 0≤n≤100
-Output: integer x
+// Directions: Given a factorial n!
+// Find the sum of its digits, and the number of trailing zeros (ie: the number of zeros at the end of the factorial). 
 
-, where x is the number of the zeros in n!
+// Input: user chooses integer n, where 0≤n≤100
+// Output: integer x, where x is the number of the zeros in n!
 
-Example: Consider the factorial:  5! = 5x4x3x2x1 = 120.  The sum of its digits is 1+2+0 = 3, and the number of zeros at the end of 5! = 120 is 1.  Then here, x = 1 and y = 3.
+// Example: Consider the factorial:  5! = 5x4x3x2x1 = 120.  The sum of its digits is 1+2+0 = 3, and the number of zeros at the end of 5! = 120 is 1.  Then here, x = 1 and y = 3.
 
-Extra Credit:  integer y
-, where y is the sum of the digits in n!
+// Extra Credit:  integer y, where y is the sum of the digits in n!
